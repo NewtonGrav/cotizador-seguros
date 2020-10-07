b3e0e6beb39d76dea005e925fc85cb8c9d887cd7
Gonzalo <gonzalom97@outlook.com>
2020-10-07 12:26:22 -0300

    feat: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header'
 
 function App() {
   return (
 		<div>
-			<h1>App</h1>
+			<Header titulo="Cotizador de seguros" />
 		</div>
 	);
 }
